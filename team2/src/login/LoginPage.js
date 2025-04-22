f089d5f77e3956275eaf3074bb345e876b97505d
msw-Hub <swmoon7@naver.com>
2025-04-22 17:38:02 +0900

    feat : login start
    login start

---
+++ team2/src/login/LoginPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Logo />
+      <div>로그인 폼</div>
+      <Navigation />
+      <Footer />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ team2/src/login/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = () => {
+  return <div>네비게이션 바</div>;
+};
+
+export default Navigation;
