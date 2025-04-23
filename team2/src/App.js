3af167e04af80ea11cef191aad1c0ca4cb10a2ff
msw-Hub <swmoon7@naver.com>
2025-04-23 15:24:08 +0900

    fix : 라우터 수정

--- team2/src/App.js
+++ team2/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"; // 로그인 페이지 컴포넌트
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 };
 
